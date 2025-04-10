04e551455631eac24134cc70f9fbe1328cad8ac2
vineshshetty27 <vineshshetty2004@gmail.com>
2025-04-10 16:34:00 +0530

    Create knapsackgreedy.c

---
+++ knapsackgreedy.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#define MAX_ITEMS 10
+int knapsack(int w,int wt[],int val[],int n);
+int MAX(int a,int b);
+int main()
+{
+int i;
+int n;
+int W;
+int wt[MAX_ITEMS];
+int val[MAX_ITEMS];
+printf("enter the number of items:");
+scanf("%d",&n);
+printf("enter the weight of the item:\n");
+for(i=1;i<=n;i++)
+{
+scanf("%d",&wt[i]);
+}
+printf("enter the value of items:\n");
+for(i=1;i<=n;i++)
+{
+scanf("%d",&val[i]);
+}
+printf("enter the capacity of the knapsack:\n");
+scanf("%d",&W);
+printf("the maximum value in knapsack of capacity%d is:%d\n",W,knapsack(W,wt,val,n));
+return 0;
+}
+int knapsack(int W,int wt[],int val[],int n)
+{
+int i,j;
+int v[MAX_ITEMS+1][W+1];
+for(i=0;i<=n;i++)
+{
+for(j=0;j<=W;j++)
+{
+if(i==0||j==0)
+{
+v[i][j]=0;
+}
+else if(j-wt[i]<0)
+{
+v[i][j]=v[i-1][j];
+}
+else
+{
+v[i][j]=max(v[i-1][j],val[i]+v[i-1][j-wt[i]]);
+}
+}
+}
+return v[n][W];
+}
+int max(int a,int b)
+{
+return(a>b)?a:b;
+}      
